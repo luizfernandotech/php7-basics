36739c556de7c345bd2a3add84354963e197c7e6
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2019-03-31 12:47:28 -0300

    Lecture - Class fields and methods

--- public/index.php
+++ public/index.php
@@ -6,12 +6,21 @@ use App\Format\JSON;
 use App\Format\XML;
 use App\Format\YAML;
 
-$json = new JSON();
+$data = [
+    "key" => "value",
+    "another_key" => "another_value"
+];
+
+$json = new JSON($data);
 $xml = new XML();
 $yaml = new YAML();
 
-print_r('Namespaces');
+print_r("Class fields and methods\n\n");
 
-print_r($json);
-print_r($xml);
-print_r($yaml);
+var_dump($json);
+var_dump($xml);
+var_dump($yaml);
+
+var_dump($json->convert());
+var_dump(JSON::DATA);
+var_dump(JSON::convertData());
--- src/Format/JSON.php
+++ src/Format/JSON.php
@@ -3,5 +3,33 @@
 namespace App\Format;
 
 class JSON {
-    
+
+    // It's a good practice contants name is UPERCASE
+    // static variables can be accessed without need a instance
+    const DATA = [
+        "success" => true
+    ];
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function convert()
+    {
+        return json_encode(
+            array_merge(
+                self::DATA,
+                $this->data
+            )
+        );
+    }
+
+    // static methods can be accessed without need a instance
+    public static function convertData()
+    {
+        return json_encode(self::DATA);
+    }
 }
