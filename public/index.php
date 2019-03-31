14948361cec42a7c33350538f77491b2e3b671cc
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2019-03-31 12:23:15 -0300

    Lecture - Namespaces by single line

--- public/index.php
+++ public/index.php
@@ -2,9 +2,12 @@
 
 require __DIR__. '/../vendor/autoload.php';
 
-use App\Format\JSON;
-use App\Format\XML;
-use App\Format\YAML;
+//use App\Format\JSON;
+//use App\Format\XML;
+//use App\Format\YAML;
+
+use App\Format\{JSON,XML,YAML};
+
 
 $json = new JSON();
 $xml = new XML();
