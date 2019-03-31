38a1654ee1eabcedbc62b8adfdad71667544de92
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2019-03-31 12:26:47 -0300

    Lecture - Namespaces

--- public/index.php
+++ public/index.php
@@ -2,18 +2,13 @@
 
 require __DIR__. '/../vendor/autoload.php';
 
-//use App\Format\JSON;
-//use App\Format\XML;
-//use App\Format\YAML;
+use App\Format\JSON;
+use App\Format\XML;
+use App\Format\YAML;
 
-//use App\Format\{JSON,XML,YAML};
-
-//use App\Format as F;
-
-
-$json = new \App\Format\JSON();
-$xml = new \App\Format\XML();
-$yaml = new \App\Format\YAML();
+$json = new JSON();
+$xml = new XML();
+$yaml = new YAML();
 
 print_r('Namespaces');
 
